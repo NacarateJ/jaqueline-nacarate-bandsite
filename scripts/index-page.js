15fa1e5dda7305bdf5ace116c5b8a96da15e2458
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-10-27 16:39:07 -0700

    api test

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,23 +1,23 @@
-const comments = [
-  {
-    name: "Connor Walton",
-    date: "02/17/2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Emilie Beach",
-    date: "01/09/2021",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-];
+// const comments = [
+//   {
+//     name: "Connor Walton",
+//     date: "02/17/2021",
+//     comment:
+//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+//   },
+//   {
+//     name: "Emilie Beach",
+//     date: "01/09/2021",
+//     comment:
+//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+//   },
+//   {
+//     name: "Miles Acosta",
+//     date: "12/20/2020",
+//     comment:
+//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//   },
+// ];
 
 // NOT WORKING :(
 // const clearError = (commentsAddForm, commentsAddInput, commentsAddError) => {
@@ -39,6 +39,26 @@ const comments = [
 //     2000
 //   );
 // };
+
+const comments = [];
+
+// const getComments = (comments) => {
+//   const commentsPromise = axios.get(
+//     "https://project-1-api.herokuapp.com/comments/?api_key=66a6f4bd-1089-49e6-9d8d-2a42b30e913c"
+//   );
+//   commentsPromise.then((response) => {
+//     console.log(response.data);
+//   });
+// };
+
+// axios.post(
+//   "https://project-1-api.herokuapp.com/comments/?api_key=66a6f4bd-1089-49e6-9d8d-2a42b30e913c/200",
+//   {
+//     name: "Connor Walton",
+//     comment:
+//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+//   }
+// );
 
 const addComment = (event) => {
   event.preventDefault();
@@ -101,13 +121,34 @@ const displayComments = (commentsObj, commentsContainer) => {
   commentsWrapper.append(commentsName, commentsDate);
 };
 
-const display = () => {
+function displayComments() {
   const commentsContainer = document.querySelector(".comments__container");
   commentsContainer.innerHTML = "";
+
+  const getComments = (comments) => {
+    axios
+      .get(
+        "https://project-1-api.herokuapp.com/comments/?api_key=66a6f4bd-1089-49e6-9d8d-2a42b30e913c"
+      )
+
+      .then((response) => {
+        console.log(response.data);
+        display(response.data);
+      });
+  };
+
+  // axios.post(
+  //   "#",
+  //   {
+  //     name: "Connor Walton",
+  //     comment:
+  //       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  //   }
+  // );
 
   for (let i = 0; i < comments.length; i++) {
     displayComments(comments[i], commentsContainer);
   }
-};
+}
 
 display();
