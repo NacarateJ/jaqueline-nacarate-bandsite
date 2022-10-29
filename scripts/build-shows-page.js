8b821c0dc6e735d4ce922866bcf668abcf24aeb8
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-10-28 23:46:31 -0700

    shows.js - date, and backGroundColor fixed

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,43 +1,18 @@
-// create array
+// axios.get will give back a promise
+axios
+  .get(
+    "https://project-1-api.herokuapp.com/showdates?api_key=66a6f4bd-1089-49e6-9d8d-2a42b30e913c"
+  )
 
-// const shows = [];
-//   {
-//     date: "Mon Sept 06 2021",
-//     venue: "Ronald Lane",
-//     location: "San Francisco, CA",
-//   },
-
-//   {
-//     date: "Tue Sept 21 2021",
-//     venue: "Pier 3 East",
-//     location: "San Francisco, CA",
-//   },
-
-//   {
-//     date: "Fri Oct 15 2021",
-//     venue: "View Lounge",
-//     location: "San Francisco, CA",
-//   },
-
-//   {
-//     date: "Tue Nov 06 2021",
-//     venue: "Hyatt Agency",
-//     location: "San Francisco, CA",
-//   },
-
-//   {
-//     date: "Fri Nov 26 2021",
-//     venue: "Moscow Center",
-//     location: "San Francisco, CA",
-//   },
-
-//   {
-//     date: "Wed Dec 15 2021",
-//     venue: "Press Club",
-//     location: "San Francisco, CA",
-//     Button: "BUY TICKETS",
-//   },
-// ];
+  // .then handles the promise - success or error
+  .then((response) => {
+    display(response.data);
+    eventListener();
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const elementMaker = (type, className, text) => {
   const element = document.createElement(type);
@@ -49,8 +24,8 @@ const elementMaker = (type, className, text) => {
   return element;
 };
 
-// Function to render my show items
-const renderShows = (showsObj, showsSection) => {
+// Function to render shows cards
+const displayShows = (showsObj, showsSection) => {
   const showsCards = elementMaker("div", "shows__cards");
   const showsWrapper = elementMaker("div", "shows__wrapper");
   const showsLabel = elementMaker("p", "shows__label", "DATE");
@@ -61,9 +36,13 @@ const renderShows = (showsObj, showsSection) => {
   const showsWrapper3 = elementMaker("div", "shows__wrapper");
   const showsLabel3 = elementMaker("p", "shows__label", "LOCATION");
   showsLabel3.classList.add("shows__label--hidden");
-  const showsInfo = elementMaker("p", "shows__info", showsObj.date);
+  const showsInfo = elementMaker(
+    "p",
+    "shows__info",
+    showsDate(new Date(showsObj.date))
+  );
   showsInfo.classList.add("shows__info--bold");
-  const showsInfo2 = elementMaker("p", "shows__info", showsObj.venue);
+  const showsInfo2 = elementMaker("p", "shows__info", showsObj.place);
   const showsInfo3 = elementMaker("p", "shows__info", showsObj.location);
   const showsButton = elementMaker("button", "shows__button", "BUY TICKETS");
 
@@ -75,83 +54,64 @@ const renderShows = (showsObj, showsSection) => {
   showsWrapper3.append(showsLabel3, showsInfo3);
 };
 
-// Function to render the entire shows section
-const apiShows = () => {
-  axios
-    .get(
-      "https://project-1-api.herokuapp.com/showdates?api_key=66a6f4bd-1089-49e6-9d8d-2a42b30e913c"
-    )
-    .then((response) => {
-      apiArray(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
+const showsContainer = elementMaker("div", "shows__container");
+const showsHiddenLabels = elementMaker("div", "shows__hidden-labels");
+const showsLabelHidden = elementMaker("p", "shows__label", "DATE");
+const showsLabel2Hidden = elementMaker("p", "shows__label", "VENUE");
+const showsLabel3Hidden = elementMaker("p", "shows__label", "LOCATION");
+
+showsContainer.append(showsHiddenLabels);
+showsHiddenLabels.append(
+  showsLabelHidden,
+  showsLabel2Hidden,
+  showsLabel3Hidden
+);
+
+const showsSection = document.querySelector(".shows"); // To get shows container
+showsSection.innerHTML = ""; // To clean the section
+
+const showsTitle = document.createElement("h2");
+showsTitle.classList.add("shows__title");
+showsTitle.innerText = "Shows";
+showsSection.append(showsTitle);
+
+// Function to render apiArray
+const display = (apiArray) => {
+  // Function to loop through each item of my array
+  for (let i = 0; i < apiArray.length; i++) {
+    displayShows(apiArray[i], showsSection);
+  }
+};
+
+// Function to add backGroundColor to shows cards onclick
+const eventListener = () => {
+  const showsCardsBackGround = document.querySelectorAll(".shows__cards");
+  for (let i = 0; i < showsCardsBackGround.length; i++) {
+    const eachShowsCardsBackGround = showsCardsBackGround[i];
+    eachShowsCardsBackGround.addEventListener("click", (event) => {
+      event.preventDefault();
+      handleClick(event);
     });
+  }
+  function handleClick(event) {
+    event.preventDefault();
+    for (let i = 0; i < showsCardsBackGround.length; i++) {
+      showsCardsBackGround[i].classList.remove("shows__cards--selected");
+      showsCardsBackGround[i].classList.remove("nohover");
+    }
+    event.currentTarget.classList.add("shows__cards--selected");
+    event.currentTarget.classList.add("nohover");
+  }
 };
 
-const localArray = [];
-
-const apiArray = (arr) => {
- for(let i = 0; i < arr.length; i++) {
-  localArray[i] = arr[i];
- }
+// Function to fix date
+function showsDate(date) {
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    day: "2-digit",
+  };
+  return date.toLocaleDateString("en-ca", options).replace(/,/g, "");
 }
-console.log(localArray)
- const showsContainer = elementMaker("div", "shows__container");
- const showsHiddenLabels = elementMaker("div", "shows__hidden-labels");
- const showsLabelHidden = elementMaker("p", "shows__label", "DATE");
- const showsLabel2Hidden = elementMaker("p", "shows__label", "VENUE");
- const showsLabel3Hidden = elementMaker("p", "shows__label", "LOCATION");
-
- showsContainer.append(showsHiddenLabels);
- showsHiddenLabels.append(
-   showsLabelHidden,
-   showsLabel2Hidden,
-   showsLabel3Hidden
- );
-
-const render = (showsContainer) => {
-for(let i = 0; i < localArray.length; i++) {
-  renderShows(localArray[i], showsContainer)
-  console.log(showsContainer)
-}
-  // showsData.forEach((showsObj) => {
-  //   renderShows(showsObj, showsContainer);
-  //  console.log(showsContainer);
-    
-  // });
-};
-
-render(showsContainer);
-
-// const showsSection = document.querySelector(".shows"); // To get shows container
-// showsSection.innerHTML = ""; // To clean the section
-
-// const showsTitle = document.createElement("h2");
-// showsTitle.classList.add("shows__title");
-// showsTitle.innerText = "Shows";
-// showsSection.append(showsTitle);
-
-// const showsCardsBackGround = document.querySelectorAll(".shows__cards");
-
-// for (let i = 0; i < showsCardsBackGround.length; i++) {
-//   const eachShowsCardsBackGround = showsCardsBackGround[i];
-
-//   eachShowsCardsBackGround.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     handleClick(event);
-//   });
-// }
-
-// function handleClick(event) {
-//   event.preventDefault();
-//   for (let i = 0; i < showsCardsBackGround.length; i++) {
-//     showsCardsBackGround[i].classList.remove("shows__cards--selected");
-//     showsCardsBackGround[i].classList.remove("nohover");
-//   }
-
-//   event.currentTarget.classList.add("shows__cards--selected");
-//   event.currentTarget.classList.add("nohover");
-// }
-
-apiShows();
